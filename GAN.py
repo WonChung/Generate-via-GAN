2f3670b890bb3b1f0bf25d9eab179af6e2237cf9
WonChung <wchung3@swarthmore.edu>
2017-05-22 21:43:44 -0400

    started with creating images

--- GAN.py
+++ GAN.py
@@ -1,2 +1,16 @@
 # GAN to generate handwritten and printed images
 # Generate links, dates, times
+
+import tensorflow as tf
+import numpy as np
+import os
+import subprocess
+
+def main():
+    createImage()
+
+def createImage(size, text, name, directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+main()
